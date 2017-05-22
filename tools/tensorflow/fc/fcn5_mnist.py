dcd3b1a413b87fae8085e0cd131dcfa638545958
tfboyd <tobyboyd@google.com>
2017-05-21 20:26:25 -0700

    Added datasets as an option

--- tools/tensorflow/fc/fcn5_mnist.py
+++ tools/tensorflow/fc/fcn5_mnist.py
@@ -56,21 +56,48 @@ def train(model='fcn5'):
     config = tf.ConfigProto(allow_soft_placement=False,log_device_placement=FLAGS.log_device_placement)
     device_id = FLAGS.device_id
     device_str = ''
+    # Try DataSets
+    try_datasets = False
     if int(device_id) >= 0:
         device_str = '/gpu:%d'%int(device_id)
     else:
         device_str = '/cpu:0'
         num_threads = os.getenv('OMP_NUM_THREADS', 1)
         config = tf.ConfigProto(allow_soft_placement=True, intra_op_parallelism_threads=int(num_threads))
-
-
+    config = tf.ConfigProto(allow_soft_placement=True,intra_op_parallelism_threads=1,inter_op_parallelism_threads=0)
+    config.gpu_options.force_gpu_compatible = True
+    os.environ['TF_ENABLE_WINOGRAD_NONFUSED'] = '1'
+    #config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
+    
     with tf.Graph().as_default(), tf.device(device_str), tf.Session(config=config) as sess:
         feature_dim = models.feature_dim
         label_dim = models.label_dim
-        images = tf.placeholder(tf.float32, [None, feature_dim])
-        labels = tf.placeholder(tf.float32, [None, label_dim])
+        images = None
+        labels = None
+
+        iterator = None
+        if try_datasets:
+            with tf.device('/CPU:0'):
+                d_features = mnist.train.images
+                d_labels = mnist.train.labels
+                # First 5,000 are labels
+                dataset = tf.contrib.data.Dataset.from_tensor_slices((d_features[5000:], d_labels[5000:]))
+                dataset = dataset.repeat()
+                dataset = dataset.shuffle(buffer_size=50000)
+                dataset = dataset.batch(FLAGS.batch_size)
+                iterator = dataset.make_initializable_iterator()
+
+                #iterator = dataset.make_one_shot_iterator()
+                images,labels = iterator.get_next()
+                
+
+        else:
+            images = tf.placeholder(tf.float32, [None, feature_dim], name="images_placeholder")
+            labels = tf.placeholder(tf.float32, [None, label_dim], name="labels_placeholder")
+        print("shape:{}".format(images))
 
         logits = None
+        loss = None
         if model == 'fcn5':
             logits = models.model_fcn5(images)
         else:
@@ -85,8 +112,14 @@ def train(model='fcn5'):
         optimizer = tf.train.MomentumOptimizer(lr, 0.9).minimize(loss)
 
         init = tf.global_variables_initializer()
+
+       
+
         sess.run(init)
-        tf.train.start_queue_runners(sess=sess)
+        if try_datasets:
+            #print('image cnt:{} label cnt:{}'.format(d_features.shape,d_labels.shape))
+            sess.run(iterator.initializer)
+        #tf.train.start_queue_runners(sess=sess)
         batch_size_per_epoch = int((EPOCH_SIZE + FLAGS.batch_size - 1)/ FLAGS.batch_size)
         iterations = FLAGS.epochs * batch_size_per_epoch 
         average_batch_time = 0.0
@@ -94,9 +127,16 @@ def train(model='fcn5'):
         average_loss = 0.0
         for step in range(iterations):
             start_time = time.time()
-            imgs, labs = get_real_batch_data(FLAGS.batch_size, 10)
-            _, loss_value = sess.run([optimizer, loss], feed_dict={images:imgs,labels:labs})
+            imgs = None
+            labs = None
+            if not try_datasets:
+                imgs, labs = get_real_batch_data(FLAGS.batch_size, 10)
+            if try_datasets:
+                _, loss_value = sess.run([optimizer, loss])
+            else:
+                _, loss_value = sess.run([optimizer, loss], feed_dict={images:imgs,labels:labs})
             average_loss += loss_value
+            #print ("HERE:{}".format(loss_value))
             duration = time.time() - start_time
             average_batch_time += float(duration)
             assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
@@ -105,12 +145,12 @@ def train(model='fcn5'):
                 sec_per_batch = float(duration)
                 format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f sec/batch)')
                 print (format_str % (datetime.now(), step, loss_value, examples_per_sec, sec_per_batch))
-            if step > 0 and step % (FLAGS.eval_step * batch_size_per_epoch) == 0:
-                average_loss /= FLAGS.eval_step * batch_size_per_epoch
-                accuracy_value = accuracy.eval(feed_dict={images: mnist.test.images, labels: mnist.test.labels})
-                print("test accuracy %g"%accuracy_value)
-                epochs_info.append('%d:%g:%s'%(step/(FLAGS.eval_step*batch_size_per_epoch), accuracy_value, average_loss)) 
-                average_loss = 0.0
+            #if step > 0 and step % (FLAGS.eval_step * batch_size_per_epoch) == 0:
+            #    average_loss /= FLAGS.eval_step * batch_size_per_epoch
+            #    accuracy_value = accuracy.eval(feed_dict={images: mnist.test.images, labels: mnist.test.labels})
+            #    print("test accuracy %g"%accuracy_value)
+            #    epochs_info.append('%d:%g:%s'%(step/(FLAGS.eval_step*batch_size_per_epoch), accuracy_value, average_loss)) 
+            #    average_loss = 0.0
         average_batch_time /= iterations
         print 'average_batch_time: ', average_batch_time
         print ('epoch_info: %s' % ','.join(epochs_info))
